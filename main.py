d6ad0cd813220582bc20cf5c57bfc943e7cd4d9d
DEB <112969476+Mot932@users.noreply.github.com>
2023-11-18 19:18:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import keyboard
 import os
 import random
-import time
 
 COLS = 25
 ROWS = 10
 EMPTY = '☐'
 PLAYER = 'P'
-ANTHILL = 'A'
 ANT = 'a'
 
 class Cell:
+    """
+    Класс Cell представляет отдельную ячейку на игровом поле.
+
+    Attributes:
+        image (str): Символ, представляющий изображение ячейки.
+        Y (int): Координата Y ячейки на поле.
+        X (int): Координата X ячейки на поле.
+        content (object): Содержимое ячейки, такое как игрок или муравей.
+    """
+
     def __init__(self, Y=None, X=None):
+        """
+        Инициализация объекта Cell.
+
+        Parameters:
+            Y (int): Координата Y ячейки на поле.
+            X (int): Координата X ячейки на поле.
+        """
         self.image = EMPTY
         self.Y = Y
         self.X = X
         self.content = None
 
     def draw(self):
+        """
+        Отображение содержимого ячейки.
+        Если ячейка пуста, отображается символ EMPTY, иначе отображается символ содержимого.
+        """
         if self.content:
             print(self.content.image, end=' ')
         else:
@@ -25,21 +44,44 @@ class Cell:
 
 
 class Player:
+    """
+    Класс Player представляет игрока на поле.
+
+    Attributes:
+        image (str): Символ, представляющий изображение игрока.
+        Y (int): Координата Y игрока на поле.
+        X (int): Координата X игрока на поле.
+    """
+
     def __init__(self, Y=None, X=None):
+        """
+        Инициализация объекта Player.
+
+        Parameters:
+            Y (int): Координата Y игрока на поле.
+            X (int): Координата X игрока на поле.
+        """
         self.image = PLAYER
         self.Y = Y
         self.X = X
 
     def move(self, direction, field):
+        """
+        Перемещение игрока на поле в указанном направлении.
+
+        Parameters:
+            direction (str): Направление движения ('up', 'down', 'left', 'right').
+            field (Field): Объект поля, на котором находится игрок.
+        """
         new_Y, new_X = self.Y, self.X
 
-        if direction == 'up' and self.Y > 0 and field.cells[self.Y - 1][self.X].content != Anthill:
+        if direction == 'up' and self.Y > 0:
             new_Y -= 1
-        elif direction == 'down' and self.Y < field.rows - 1 and field.cells[self.Y + 1][self.X].content != Anthill:
+        elif direction == 'down' and self.Y < field.rows - 1:
             new_Y += 1
-        elif direction == 'left' and self.X > 0 and field.cells[self.Y][self.X - 1].content != Anthill:
+        elif direction == 'left' and self.X > 0:
             new_X -= 1
-        elif direction == 'right' and self.X < field.cols - 1 and field.cells[self.Y][self.X + 1].content != Anthill:
+        elif direction == 'right' and self.X < field.cols - 1:
             new_X += 1
 
         if field.cells[new_Y][new_X].content is None:
@@ -48,93 +90,162 @@ class Player:
             field.cells[self.Y][self.X].content = self
 
 
-class Anthill:
-    def __init__(self, Y=None, X=None):
-        self.image = ANTHILL
-        self.Y = Y
-        self.X = X
-
-    def spawn_ant(self, field):
-        ant = Ant(self.Y, self.X)
-        field.cells[self.Y][self.X].content = ant
-        return ant
-
-
 class Ant:
+    """
+    Класс Ant представляет муравья на поле.
+
+    Attributes:
+        image (str): Символ, представляющий изображение муравья.
+        Y (int): Координата Y муравья на поле.
+        X (int): Координата X муравья на поле.
+    """
+
     def __init__(self, Y=None, X=None):
+        """
+        Инициализация объекта Ant.
+
+        Parameters:
+            Y (int): Координата Y муравья на поле.
+            X (int): Координата X муравья на поле.
+        """
         self.image = ANT
         self.Y = Y
         self.X = X
 
     def move_away_from_player(self, player, field):
+        """
+        Движение муравья в противоположном направлении относительно игрока.
+
+        Parameters:
+            player (Player): Объект игрока на поле.
+            field (Field): Объект поля, на котором находится муравей.
+        """
         directions = ['up', 'down', 'left', 'right']
-        random.shuffle(directions)
+        opposite_directions = {
+            'up': 'down',
+            'down': 'up',
+            'left': 'right',
+            'right': 'left'
+        }
 
-        for direction in directions:
-            new_Y, new_X = self.Y, self.X
+        direction = random.choice(directions)
+        player_distance = abs(player.Y - self.Y) + abs(player.X - self.X)
+        new_Y, new_X = self.Y, self.X
 
-            if direction == 'up' and self.Y > 0:
+        while True:
+            if direction == 'up' and new_Y > 0:
                 new_Y -= 1
-            elif direction == 'down' and self.Y < field.rows - 1:
+            elif direction == 'down' and new_Y < field.rows - 1:
                 new_Y += 1
-            elif direction == 'left' and self.X > 0:
+            elif direction == 'left' and new_X > 0:
                 new_X -= 1
-            elif direction == 'right' and self.X < field.cols - 1:
+            elif direction == 'right' and new_X < field.cols - 1:
                 new_X += 1
 
-            if field.cells[new_Y][new_X].content is None:
-                field.cells[self.Y][self.X].content = None
-                self.Y, self.X = new_Y, new_X
-                field.cells[self.Y][self.X].content = self
+            new_distance = abs(player.Y - new_Y) + abs(player.X - new_X)
+            if new_distance >= player_distance:
                 break
 
+            direction = opposite_directions[direction]
+
+        if field.cells[new_Y][new_X].content is None:
+            field.cells[self.Y][self.X].content = None
+            self.Y, self.X = new_Y, new_X
+            field.cells[self.Y][self.X].content = self
 
 class Field:
-    def __init__(self, cell=Cell, player=Player, anthill=Anthill, ant=Ant):
+    """
+    Класс Field представляет игровое поле.
+
+    Attributes:
+        rows (int): Количество строк на поле.
+        cols (int): Количество столбцов на поле.
+        cells (list): Двумерный список объектов Cell, представляющих ячейки поля.
+        player (Player): Объект игрока на поле.
+        ant (Ant): Объект муравья на поле.
+    """
+
+    def __init__(self, cell=Cell, player=Player, ant=Ant):
+        """
+        Инициализация объекта Field.
+
+        Parameters:
+            cell (Cell): Класс, представляющий ячейку на поле.
+            player (Player): Класс, представляющий игрока на поле.
+            ant (Ant): Класс, представляющий муравья на поле.
+        """
         self.rows = ROWS
         self.cols = COLS
         self.cells = [[cell(Y=y, X=x) for x in range(COLS)] for y in range(ROWS)]
         self.player = player(Y=random.randint(0, ROWS - 1), X=random.randint(0, COLS - 1))
-        self.anthill = anthill(Y=random.randint(0, ROWS - 1), X=random.randint(0, COLS - 1))
+        self.ant = ant(Y=random.randint(0, ROWS - 1), X=random.randint(0, COLS - 1))
         self.cells[self.player.Y][self.player.X].content = self.player
-        self.cells[self.anthill.Y][self.anthill.X].content = self.anthill
+        self.cells[self.ant.Y][self.ant.X].content = self.ant
 
     def drawrows(self):
+        """
+        Отображение всех строк игрового поля.
+        """
         for row in self.cells:
             for cell in row:
                 cell.draw()
             print()
 
 
+def clear_screen():
+    """
+    Очистка экрана консоли, учитывая операционную систему.
+    """
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
 class Game:
+    """
+    Класс Game представляет игровой процесс.
+
+    Attributes:
+        field (Field): Объект игрового поля.
+    """
+
     def __init__(self):
+        """
+        Инициализация объекта Game.
+        """
         self.field = Field()
 
-    def clear_screen(self):
-        if os.name == 'nt':
-            os.system('cls')
-        else:
-            os.system('clear')
-
     def run(self):
-        self.clear_screen()
+        """
+        Запуск игрового процесса.
+        """
         self.field.drawrows()
 
         while True:
             event = keyboard.read_event(suppress=True)
             if event.event_type == keyboard.KEY_DOWN:
-                if event.name == 'up' or event.name == 'down' or event.name == 'left' or event.name == 'right':
-                    self.field.player.move(event.name, self.field)
+                if event.name == 'up':
+                    self.field.player.move('up', self.field)
+                    self.field.ant.move_away_from_player(self.field.player, self.field)
+                    clear_screen()
+                    self.field.drawrows()
+                elif event.name == 'down':
+                    self.field.player.move('down', self.field)
+                    self.field.ant.move_away_from_player(self.field.player, self.field)
+                    clear_screen()
+                    self.field.drawrows()
+                elif event.name == 'left':
+                    self.field.player.move('left', self.field)
+                    self.field.ant.move_away_from_player(self.field.player, self.field)
+                    clear_screen()
+                    self.field.drawrows()
+                elif event.name == 'right':
+                    self.field.player.move('right', self.field)
+                    self.field.ant.move_away_from_player(self.field.player, self.field)
+                    clear_screen()
+                    self.field.drawrows()
+                    
 
-                    if random.random() < 0.2:  # 20% chance to spawn an ant
-                        ant = self.field.anthill.spawn_ant(self.field)
-                        time.sleep(0.5)  # Delay to avoid spawning multiple ants at once
-                        ant.move_away_from_player(self.field.player, self.field)
-
-                self.clear_screen()
-                self.field.drawrows()
-
-
-# Create an instance of the Game class and run the game loop
-my_game = Game()
-my_game.run()
+game_instance = Game()
+game_instance.run()
