f7b4e1b90c4e5ba222557e5967ab89c35f09e1ae
DEB <112969476+Mot932@users.noreply.github.com>
2023-11-24 17:58:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,54 @@ import keyboard
 import os
 import random
 
-
 COLS = 25
 ROWS = 10
 EMPTY = '☐'
 PLAYER = 'P'
 ANT = 'a'
 ANTHILL = 'A'
-ANTHILL_MAX = 4
-ANTHILL_MINI = 1
 UP = 'up'
 DOWN = 'down'
 RIGHT = 'right'
 LEFT = 'left'
+ANTHILL_MAX = 4
+ANTHILL_MINI = 1
 
 
 class GameObject:
-    def __init__(self, y, x):
+    def __init__(self, y, x, image):
         self.y = y
         self.x = x
-        self.image = None
+        self.image = image
+
+    def move(self, direction, field):
+        new_y, new_x = self.y, self.x
+
+        if direction == UP and self.y > 0 and not isinstance(field.cells[self.y - 1][self.x].content, Anthill):
+            new_y -= 1
+        elif direction == DOWN and self.y < field.rows - 1 and not isinstance(field.cells[self.y + 1][self.x].content, Anthill):
+            new_y += 1
+        elif direction == LEFT and self.x > 0 and not isinstance(field.cells[self.y][self.x - 1].content, Anthill):
+            new_x -= 1
+        elif direction == RIGHT and self.x < field.cols - 1 and not isinstance(field.cells[self.y][self.x + 1].content, Anthill):
+            new_x += 1
+
+        field.cells[self.y][self.x].content = None
+        self.y, self.x = new_y, new_x
+        field.cells[self.y][self.x].content = self
+
+    def place(self, field):
+        if field.cells[self.y][self.x].content is None:
+            field.cells[self.y][self.x].content = self
+        else:
+            empty_cells = [(i, j) for i in range(field.rows) for j in range(field.cols) if field.cells[i][j].content is None]
+            if empty_cells:
+                new_y, new_x = random.choice(empty_cells)
+                field.cells[new_y][new_x].content = self
+                self.y, self.x = new_y, new_x
+
+    def draw(self, field):
+        field.cells[self.y][self.x].content = self
 
 
 class Cell:
@@ -40,45 +68,34 @@ class Cell:
 
 class Player(GameObject):
     def __init__(self, y=None, x=None):
-        super().__init__(y, x)
-        self.image = PLAYER
+        super().__init__(y, x, PLAYER)
 
     def move(self, direction, field):
-        new_y, new_x = self.y, self.x
-
-        if direction == UP and self.y > 0 and not isinstance(field.cells[self.y - 1][self.x].content, Anthill):
-            new_y -= 1
-        elif direction == DOWN and self.y < field.rows - 1 and not isinstance(field.cells[self.y + 1][self.x].content, Anthill):
-            new_y += 1
-        elif direction == LEFT and self.x > 0 and not isinstance(field.cells[self.y][self.x - 1].content, Anthill):
-            new_x -= 1
-        elif direction == RIGHT and self.x < field.cols - 1 and not isinstance(field.cells[self.y][self.x + 1].content, Anthill):
-            new_x += 1
-
-        field.cells[self.y][self.x].content = None
-        self.y, self.x = new_y, new_x
-        field.cells[self.y][self.x].content = self
+        super().move(direction, field)
 
 
 class Anthill(GameObject):
     def __init__(self, x, y, quantity):
-        super().__init__(y, x)
-        self.image = 'A'
+        super().__init__(y, x, ANTHILL)
         self.quantity = quantity
 
-    def place_anthill(self, field):
-        field.cells[self.y][self.x].content = self
+    def place(self, field):
+        super().place(field)
+
+    def draw(self, field):
+        super().draw(field)
 
 
 class Field:
-    def __init__(self, cell=Cell, player=Player, anthill=Anthill, anthill_max=ANTHILL_MAX, anthill_mini=ANTHILL_MINI):
+    def __init__(self, cell=Cell, player=Player, anthill=Anthill):
         self.rows = ROWS
         self.cols = COLS
         self.anthills = []
         self.cells = [[cell(Y=y, X=x) for x in range(COLS)] for y in range(ROWS)]
         self.player = player(y=random.randint(0, ROWS - 1), x=random.randint(0, COLS - 1))
-        self.cells[self.player.y][self.player.x].content = self.player
-
+        self.player.place(self)
+        self.player.draw(self)
+        
     def drawrows(self):
         for row in self.cells:
             for cell in row:
@@ -87,17 +104,21 @@ class Field:
 
     def add_anthill(self, anthill):
         self.anthills.append(anthill)
-        anthill.place_anthill(self)
+        anthill.place(self)
 
     def add_anthills_randomly(self):
-        for i in range(random.randint(ANTHILL_MINI, ANTHILL_MAX)):
-            anthill_x, anthill_y = random.randint(0, COLS - 1), random.randint(0, ROWS - 1)
-            while (anthill_x, anthill_y) == (self.player.x, self.player.y):
-                anthill_x, anthill_y = random.randint(0, COLS - 1), random.randint(0, ROWS - 1)
+        available_cells = [(x, y) for x in range(self.cols) for y in range(self.rows) if (x, y) != (self.player.x, self.player.y)]
+
+        quantity = random.randint(ANTHILL_MINI, ANTHILL_MAX)
+        
+        for i in range(quantity):
+            if not available_cells:
+                break
+            anthill_x, anthill_y = random.choice(available_cells)
+            available_cells.remove((anthill_x, anthill_y))
 
             anthill = Anthill(x=anthill_x, y=anthill_y, quantity=random.randint(ANTHILL_MINI, ANTHILL_MAX))
             self.add_anthill(anthill)
-
 
 def clear_screen():
     if os.name == 'nt':
